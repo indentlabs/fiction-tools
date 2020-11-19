90fd90ed01e351d198a4ea74727a31e47c68da03
Andrew Brown <drusepth@gmail.com>
2020-11-19 15:42:33 -0800

    basic ui

--- src/App.js
+++ src/App.js
@@ -1,11 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
+import ToolCard from "./components/ToolCard/ToolCard.js";
 
 function App() {
   return (
-    <div className="App">
-      neat    
-    </div>
+    <Container className="App" fixed>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Typography variant="h3" component="h1" align="center" color="primary">
+            Fiction.Tools
+          </Typography>
+          <Typography align="center" paragraph="true">
+            A collection of helpful tools for authors at every stage of the writing process.
+          </Typography>
+        </Grid>
+
+        <Grid item xs={12}>
+          <Typography variant="h4" color="secondary" gutterBottom>
+            Table of Contents
+          </Typography>
+          <ul>
+            <li>Worldbuilding</li>
+          </ul>
+        </Grid>
+
+        <Grid item xs={12}>
+          <Typography variant="h4" component="h2" color="secondary" gutterBottom>
+            Worldbuilding
+          </Typography>
+        </Grid>
+        <Grid item xs={12} sm={12} md={6} lg={4}>
+          <ToolCard
+            title={"Notebook.ai"}
+            subtitle={"The smart worldbuilding notebook"}
+            square_logo={"https://www.notebook.ai/assets/logos/book-small-42459c9b5feab2a71f20540b0f3035507802ecb7f9aa5899238ed8f69e34560d.png"}
+            screenshot={"https://www.notebook.ai/assets/logos/book-small-42459c9b5feab2a71f20540b0f3035507802ecb7f9aa5899238ed8f69e34560d.png"}
+            description={`
+              Your digital notebook grows and collaborates with you as you create magnificent universes — and everything within them.
+              Create a universe (literally) and track every aspect of its characters, locations, items, and more. Our AI writing assistant asks questions about your content, helping you dive deeper than ever into your world.
+            `}
+          />
+        </Grid>
+
+      </Grid>
+    </Container>
   );
 }
 
--- src/components/ToolCard/ToolCard.js
+++ src/components/ToolCard/ToolCard.js
@@ -1,12 +1,77 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import './ToolCard.css';
 
-const ToolCard = () => (
-  <div className="ToolCard">
-    ToolCard Component
-  </div>
-);
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import Avatar from '@material-ui/core/Avatar';
+import Typography from '@material-ui/core/Typography';
+
+import Tooltip from '@material-ui/core/Tooltip';
+import Zoom from '@material-ui/core/Zoom';
+
+import IconButton from '@material-ui/core/IconButton';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+
+class ToolCard extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      highlighted: false
+    };
+  }
+
+  render() {
+    return(
+      <a href="https://www.notebook.ai" style={{textDecoration: 'none'}}>
+        <Card
+          onMouseEnter={() => { this.setState({highlighted: true}); }}
+          onMouseLeave={() => { this.setState({highlighted: false}); }}
+          elevation={this.state.highlighted ? 10 : 3}
+        >
+          <CardHeader
+            title={this.props.title}
+            subheader={this.props.subtitle}
+            avatar={<img src={this.props.square_logo} height="100" width="100" />}
+          />
+          <CardMedia
+            image={this.props.screenshot}
+            title={this.props.title}
+            style={{height: 0, paddingTop: '33.33%'}}
+          />
+          <CardContent>
+            {this.props.description.split('\n').map(function(item, key) {
+              return (
+                <Typography key={key} paragraph="true">
+                  {item}
+                </Typography>
+              )
+            })}
+          </CardContent>
+          <CardActions disableSpacing>
+            <Tooltip arrow interactive 
+                     title={
+                      <div style={{fontSize: '1.5em', padding: '10px'}}>
+                        Has a free tier
+                        </div>
+                      } 
+                     TransitionComponent={Zoom}
+            >
+              <IconButton>
+                <FavoriteIcon />
+              </IconButton>
+            </Tooltip>
+          </CardActions>
+        </Card>
+      </a>
+    )
+  }
+};
 
 ToolCard.propTypes = {};
 
